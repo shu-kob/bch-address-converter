5e3537ae55649368d7a40f7f945e1be9f4e9a9cd
小渕周 <shukobuchi@gmail.com>
2021-12-12 23:26:10 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# bch-address-converter
---
+++ bchAddressConverter.js
@@ -0,0 +1,16 @@
+import bchaddr from 'bchaddrjs';
+
+var address = process.argv[2];
+
+var isCashAddress = bchaddr.isCashAddress;
+var isLegacyAddress = bchaddr.isLegacyAddress;
+var toLegacyAddress = bchaddr.toLegacyAddress;
+var toCashAddress  = bchaddr.toCashAddress;
+
+if (isCashAddress(address)) {
+  var legacy_address = toLegacyAddress(address);
+  console.log(legacy_address);
+} else if (isLegacyAddress(address)) {
+  var bcash_address = toCashAddress(address);
+  console.log(bcash_address);
+}
